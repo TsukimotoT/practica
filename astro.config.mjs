04701fc14b360474355a4d8c9e06a5ff334461f7
TsukimotoT <rodrigo.moya.d@gmail.com>
2024-11-17 18:13:11 -0300

    corregir rutas

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ export default defineConfig({
   site: 'https://www.funerariaaurora.cl',
   integrations: [tailwind(), sitemap()],
   output: 'static',
-  base: '/funeraria-aurora/', // Asegúrate de usar el nombre del repositorio aquí.
-  site: 'https://TsukimotoT.github.io/funeraria-aurora/',
-});
+  base: '/practica/', // Asegúrate de usar el nombre del repositorio aquí.
+  site: 'https://tsukimotot.github.io/practica/',
+})
