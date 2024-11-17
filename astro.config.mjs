e9c15ed597724c1f156cd8ca645c596bb8be32bb
TsukimotoT <rodrigo.moya.d@gmail.com>
2024-11-17 13:59:52 -0300

    actualizando astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.funerariaaurora.cl',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  output: 'static',
+  base: '/funeraria-aurora/', // Asegúrate de usar el nombre del repositorio aquí.
+  site: 'https://TsukimotoT.github.io/funeraria-aurora/',
 });
